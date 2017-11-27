02b086b55f77e683858a83863d619bcd3eace683
Cory Lynch <cjl2625@gmail.com>
2017-11-27 02:13:55 -0500

    Add reading of votes into python.

--- submit.py
+++ submit.py
@@ -5,6 +5,7 @@ import csv
 import cgi
 import sqlite3
 import sys
+import json
 print "Content-type: text/html\nAccess-Control-Allow-Origin: *\n"
 kerb = os.environ['SSL_CLIENT_S_DN_Email'].split('@')[0]
 
@@ -14,22 +15,34 @@ with open("residents.csv", "r") as f:
 	for row in res:
 		residents[row[1]] = row[0]
 
-form = cgi.FieldStorage()
-if not 'choice' in form:
-	print "Invalid vote. Are you sure you chose a candidate?"
+def alert(title, msg): # Show error message and quit
+	with open("alert.html", "r") as f:
+		data = f.read()
+
+	print data % (title, msg)
 	sys.exit(0)
 
-vote = form['choice'].value
-if 'write' in vote:
-	if 'write' in form:
-		vote = form['write'].value
+form = cgi.FieldStorage()
+vote = []
+for i,q in enumerate(config['questions']):
+	field = 'q' + str(i)
+	write = 'write' + str(i)
+	if field in form and form['field'] != 'write':
+		vote.append(form[field])
+	elif write in form and form[write] != '':
+		vote.append(form[write])
 	else:
-		print "Invalid vote. You didn't type a write-in candidate."
+		vote.append("Abstain")
 
 if not kerb in residents:
-	print "Error: You're not a Next resident! <br/>"
-	print "If you believe you received this in error, please notify next-techchair@mit.edu"
-	sys.exit(0)
+	alert("Voting Error", "You're not a Next resident!")
+
+# Load the config file for the election
+with open('config.js') as f:
+    config = f.read()
+
+config = '\n'.join(config.split('\n')[1:]) #chop var definition
+config = json.loads(config)
 
 def get_wing(kerb):
 	room = residents[kerb]
@@ -40,18 +53,28 @@ def get_wing(kerb):
 init = not os.path.isfile("results.db")
 conn = sqlite3.connect("results.db")
 c = conn.cursor()
+
+nstr = ''
+qstr = ''
+for i,q in enumerate(config['questions']):
+	nstr += q + int(i) + ' text, '
+	qstr += '?,'
+nstr = nstr[:-2]
+qstr = qstr[:-1]
+print(nstr)
+print(qstr)
+
+sys.exit(0)
 if init:
 	c.execute("CREATE TABLE results (name text, wing text, vote text)")
 	conn.commit()
 
 voted = c.execute('SELECT * FROM results WHERE name=?', (kerb,)).fetchall()
 if len(voted):
-	print "You've already voted."
-	sys.exit(0)
+	alert("Voting Error", "You've already voted.")
 
 c.execute('INSERT INTO results VALUES (?,?,?)', (kerb, get_wing(kerb), vote))
 conn.commit()
 conn.close()
 
-print 'Your vote has been recorded!'
-print '<script>window.location = "https://next.scripts.mit.edu/vote/budget"</script>'
+alert("Success", "Your vote has been recorded.")
