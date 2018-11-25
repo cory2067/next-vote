dc246c202770f449e1621b33f48bdbb98b8d3d2f
Cory Lynch <cjl2625@gmail.com>
2018-11-25 16:02:11 -0500

    Add election for exec 2018.

---
+++ archive/config-budget2018f.js
@@ -0,0 +1,24 @@
+var config = // Important newline so python script can parse valid JSON
+{
+    "title": "Fall 2018 Budget/Wingrep Vote",
+    "intro": "Next Exec has approved the following budget for the Fall 2018 semester. Please read over the document, and vote for whether or not you approve. If you do not, please leave a comment describing what you'd like to see changed. Also, please vote for your wing's Wingreps.",
+    "document": "https://drive.google.com/file/d/1fF2obayeV1hMSmPi6ywHpnd4uTEfojEb7n4V_iRn-po/preview",
+    "questions": [
+        {
+            "title": "Do you approve of the proposed budget?",
+            "options": ["Yes", "No"]
+        },
+        {
+            "title": "Wingrep",
+            "options": [
+                {"title": "Janelle Sands, Elizabeth DeTienne, Caleb Richardson", "wing": "2E"},
+                {"title": "Jenny Gao, Jamie Gong", "wing": "2W"},
+                {"title": "Mihir Singhal, Chelsea Ajunwa, Jiayi Dong", "wing": "3E"},
+                {"title": "Andrew Lin, Swapnil Garg, Agustin Garcia", "wing": "3W"},
+                {"title": "Nick Ignacio, Luke Bastian", "wing": "4E"},
+                {"title": "Gabriel Mintzer", "wing": "4W"}
+            ],
+            "write_in": true
+        }
+    ]
+}
--- config.js
+++ config.js
@@ -1,22 +1,106 @@
 var config = // Important newline so python script can parse valid JSON
 {
-    "title": "Fall 2018 Budget/Wingrep Vote",
-    "intro": "Next Exec has approved the following budget for the Fall 2018 semester. Please read over the document, and vote for whether or not you approve. If you do not, please leave a comment describing what you'd like to see changed. Also, please vote for your wing's Wingreps.",
-    "document": "https://drive.google.com/file/d/1fF2obayeV1hMSmPi6ywHpnd4uTEfojEb7n4V_iRn-po/preview",
+    "title": "Fall 2017 Elections",
+    "intro": "Next House is holding elections for Next Exec. The candidates for each position are listed below.",
+    "document": "",
     "questions": [
         {
-            "title": "Do you approve of the proposed budget?",
-            "options": ["Yes", "No"]
+            "title": "President",
+            "options": [
+                {"title": "Jessica Tang", "description": "Hi Nexties~ I’m Jessica Tang, also known as jynnie, and I’m excited to be running for President of Next House this year! In the last two years I’ve enjoyed working with the house and exec as VP of Relations and Housing Chair and have found my passion is serving Next House. My goal as president will be to support Nexties in their endeavors. Be that pursuing facility improvements, new student groups, updating housing procedures and tools (such as the housing website), exciting social events (can we bring karaoke back?), or more snacks and swag. I also want to focus on keeping up transparent, constructive conversations both within our house and with MIT’s administration to ensure and improve Nexties’ experiences. I hope to serve as your President and support our Next House C:"},
+                {"title": "Benjamin Nguyen", "description": "Next House has been a second home for me and I want to ensure that all our residents can feel the same way. Over the past year and a half, I've served on Next Exec as both Publicity and JudComm chair, giving me an intimate understanding of the workings of Next House and Exec. My priorities as president will be using resident feedback to inform the process of creating the new makerspace, coordinating wing preference in the spring housing lottery, and collaborating with our treasurer to find ways to use our budget surplus sustainably. Outside of Next House, I'd also work to make Next's voice better heard by administration and campus-wide groups, such as Dormcon and the UA, by using my experience as a member of RingComm 2021. Together, let's make a better Next House for all of us!"}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "VP of Relations",
+            "options": [
+                {"title": "Mary Dahl", "description": "Hi Next House! I'm Mary, and I'm running for Vice President of Relations! As someone who has been on exec before as social chair, as well as being on the exec board for a number of student groups (Next Haunt, Next Big Thing, and Next Quest), I have had lots of experience working with Nexties to make cool things happen. As VP Rel, I want to continue this awesome tradition that Next House has. If a student group has a crazy idea of something big they want to do, I want to help them do it! If you and your friends want to make a project happen, we can work to do that too. Next House is a wonderfully creative place, and our student groups are where that creativity shines. I care so much about Next House and making sure that things happen -- you've probably seen me tirelessly working on Haunt, or running study breaks, or any number of other things I run around doing -- that, for my last year here, I want to be in charge of keeping it all going."}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "VP of Facilities",
+            "options": [
+                {"title": "Matt Farejowicz", "description": "Hello Next House! It's Matt, your resident VP Fac aiming to be VP Fac once more! This time around, I'd like to be as responsive to feedback as possible, and work to improve communication between students and administration as well. If you guys would like improvements in your lounges, hallways, or other common spaces, please let me know and I'll get right on it. And if there's ever an issue that seems too big or deals with existing policies, I'll make sure that we get the right conversations going so that we can make things happen."}
+            ],
+            "write_in": true
+        },
+        {
+            "title": "Treasurer",
+            "options": [
+                {"title": "John Adeyeye", "description": "Hey, I'm John Adeyeye! It would be an honor to be your Next House Treasurer. I'm running because I love this space and want to see you, the residents, empowered. If elected, I'll devote my mind, soul, and time to the position, be open to more feedback, and seek to make my duties more efficient for you. Let's make this a reality."},
+                {"title": "Jiayi Dong", "description": "Hi Nexties! As Treasurer, I will aim for maximum transparency and accountability when managing our budget and promptness when responding to reimbursements. I managed the CPW budget last year as CPW Co-Chair and also help manage the budget for my wing, so with that prior experience (and my love for fancy spreadsheets), I have confidence in being a responsible and organized Treasurer."}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "Secretary",
+            "options": [
+                {"title": "Thomas Adebiyi", "description": "As secretary, I will continue to provide regular and concise communication of the status of next exec. To do this I will write meeting minutes for every meeting and send plenty of emails of exec updates to keep the house solidly in the loop. Also, I will be sure to collect and compile comments and concerns from next residents and present them to the rest of exec during meetings to be sure that exec is in the loop with the rest of the house as well, keeping a constant communication loop between house and exec. If you would like to ask me questions, feel free to contact me at tadebiyi@ or visit me in room 241!"}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "Social Chair",
+            "options": [
+                {"title": "Faraz Masroor", "description": "Hi, I'm Faraz! I'm seriously campaigning to become your Social Chair, and I actually do want this role. Having been a REX Chair for Next this past summer, I've picked up good skills on leadership, planning events, and ordering food, and I want to do it again during the school year. I want to do more Coffeehouse socials with donuts; get y'all some Next House baseball caps/pillowcases; keep selling Exec's stockpile of shirts; and host a study break/Coffeehouse when final exams start approaching where we get into the courtyard and SCREAM our stress away. With all that said, I really want to serve you guys and make y'all happy. So please vote for me!"}
+            ],
+            "write_in": true
+        },
+        {
+            "title": "Alumni Relations Chair",
+            "options": [
+                {"title": "Bill Pinney", "description": "Next House is an amazing place, full of amazing people, doing amazing things, and it always has been. That’s what I’ve loved about living here, and that’s also why I love getting the chance to talk with the alumni. For example, did you know that the first students of Next House referred to it as ‘the 500’ or that Next Exec in the early 2000s had a ‘rainy-day fund’ made up of $10,000 cash in a shoebox? I didn’t, not until I got the chance to talk to people who were there when these things happened, and that’s what makes the Alumni Relations position so cool. As Alumni Relations Chair, I’ve spent the past year trying to increase opportunities for students and alumni to interact, most notably the end-of year alumni gathering at Next House, which I hope will become an annual feature of our calendar. I’ve also been working behind the scenes with the Alumni Association to improve our capacity to reach out to alums, whether to invite them to events, ask them for money, or just let them know about what’s going on at the best dorm on campus. As Alumni Relations Chair I hope to continue this work, to bring Nexties new and old to the table to share their memories and learn from each other. There are so many amazing memories waiting to be told, and I would love the opportunity to help tell them."}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "Housing Chair",
+            "options": [
+                {"title": "Swapnil Garg", "description": "As housing chair, I will listen to residents’ concerns about the housing lottery and reform it to best suit everyone while being as fair as possible. I have heard complaints about the current system of deferring and swaps, and arguments for and against wing preference, and I will hold public town halls to engage as many people as possible in determining the lottery process. I will also make more information available about room sizes and other factors, especially for the freshman lottery."},
+                {"title": "Jessica Tang", "description": "Hello Nexties! I’m Jessica, or jynnie, and was previously Housing Couch in 2016-2017. Having served a term already, I have a thorough understanding of the process and minute details that Housing deals with. Previously, I worked to implement the triple tier system and developed the housing website – a web app to improve organization of the housing process and facilitate lotteries (such as with live updating maps). I will continue to support and do my best in improving the housing process and also keep up communication with MIT’s administration."},
+                {"title": "Rishabh Chandra", "description": "Hi everyone! Housing can be very stressful for many people, so the role of housing chair must be executed with as much transparency and clarity as possible. To this end, I made the map application that many of you used during the last upperclassman lottery when I was moving in (tiny.cc/nextlottery18). I definitely believe that the lottery process should better emphasize wing culture and give preference to in-wing moves (one way to do this is a wing-specific lottery), but I also think it is important that the lottery is not controlled or \"gamed\" by cliques of popular friend groups at the expense of individuals. As your housing chair I would be open to these and other ideas, and would look to settle disputes as quickly and efficiently as possible."}
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "Technology Chair",
+            "options": [
+            ],
+            "write_in": true
+        },
+        {
+            "title": "Dining Chair",
+            "options": [
+            ],
+            "shuffle": true,
+            "write_in": true
+        },
+        {
+            "title": "JudComm Chair",
+            "options": [
+                {"title": "Thomas Adebiyi", "description": "As the Judical Committee chair, it is imperative to be a reliable resource for wing reps and residents for ideas on how to better their wing. As I have a couple semesters of previous wingrep experience, I am more than capable of providing beneficial advice to wingreps all over the house. In addition, I recognize the importance of encouraging and mediating conversations between different wings' wing reps, so one of my primary goals is to promote inter-wing study breaks that are entertaining and creative, creating a stronger community within the wings of Next. Therefore, as Judcomm Chair, I will be a dependable point of contact to answer questions about wing rep duties and will help generate and refine inter-wing as well as intra-wing study break plans. In the end, I hope to make Next an even friendlier place and to make the wingreps' jobs as stress- free and efficient as possible! If you would like to ask me questions, feel free to contact me at tadebiyi@ or visit me in room 241!"},
+                {"title": "Blair Anaman Williams", "description": "I would like to be JudComm because it is my desire to lead the discussion on Next House’s values and to do my part in instituting a more comprehensive support system for Nexties. As JudComm, I will safeguard the constitution that directly lays out Next House’s values and ensuring that any actions taken by next-exec align with these values while having conversations with Nexties about what our values are and should be. Furthermore, as JudComm I will further empower Wingreps to serve their communities materially, emotionally, and mentally, which would help to accomplish my goal of improving the overall wellbeing of individuals in all the wings, and Next House by extension. Lastly, I believe my track record as housing chair shows that I’m great at executing my goals (such as with NEXTploration), being responsive to the needs of the house (displayed by proposing wing preference in response to a survey, and opening the discussion to the house to hear everyone’s opinion on the issue), and caring for Next house on a deep level, all traits I believe make me well suited for this position. So vote Blair because I Care!"},
+                {"title": "Benjamin Nguyen", "description": "I want to continue being your JudComm Chair in order to make Nexties as happy as possible. As JudComm this past year, I've worked on Exec to enable wingreps to better support their wings by increasing funding for wing purchases and streamlining finals snacks ordering. If elected again, I'll continue to empower wingreps by helping them plan study breaks, working with the house budget to increase long-term wing funding, and encouraging interwing joint study breaks. Along the way, I'll make sure elections and votes occur on time to keep the house functioning as normal!"}
+            ],
+            "shuffle": true,
+            "write_in": true
         },
         {
             "title": "Wingrep",
             "options": [
-                {"title": "Janelle Sands, Elizabeth DeTienne, Caleb Richardson", "wing": "2E"},
-                {"title": "Jenny Gao, Jamie Gong", "wing": "2W"},
-                {"title": "Mihir Singhal, Chelsea Ajunwa, Jiayi Dong", "wing": "3E"},
-                {"title": "Andrew Lin, Swapnil Garg, Agustin Garcia", "wing": "3W"},
-                {"title": "Nick Ignacio, Luke Bastian", "wing": "4E"},
-                {"title": "Gabriel Mintzer", "wing": "4W"}
+                {"title": "Thomas Adebiyi, Cory Lynch, Jessica Tang", "wing": "2E"},
+                {"title": "Jenny Gao, Jamie Geng", "wing": "2W"},
+                {"title": "Mihir Singhal, Zoe Anderson, Meghana Vemulapalli", "wing": "3E"},
+                {"title": "Agustin Garcia, Andrew Lin", "wing": "3W"},
+                {"title": "Gabriel Mintzer, Zoe Sheill, Brice Huang", "wing": "4W"}
             ],
             "write_in": true
         }
